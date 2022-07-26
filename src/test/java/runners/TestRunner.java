b07b9ef43d9ac016c529288638b034da44cdadf3
cancanki <cankiosman@gmail.com>
2022-07-26 15:07:14 +0300

    runner class is changed

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,14 +1,16 @@
 package runners;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 
 @CucumberOptions(features = "src/test/resources/features",
         glue = {"steps"},
-        tags = "@Regression"
+        tags = "@Regression",
+        plugin = {"pretty", "html:target/cucumber.html"}
 )
 
 public class TestRunner {
